11988d5e912e73486376e73c87b4a9ba0081b214
Andy-Gatsby <marathi.andy@gmail.com>
2023-02-20 17:00:36 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,8 @@
 
 
 /**
- * main * prints "with proper grammer, but the outcome is 
- * a piece of art." followed newline
+ * main * prints "with proper grammar, but the outcome
+ * is a piece of art," followed newline
  * Return: Always 0.
  *
  */
